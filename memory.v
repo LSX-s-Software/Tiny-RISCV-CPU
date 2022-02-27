c2364f76febef8a880f76074a13070f7ce8cb3b2
LSX-s-Software <2682191498@qq.com>
2022-02-27 16:22:21 +0800

    refactor: rename memory.v

--- memory.v
+++ memory.v
@@ -3,8 +3,8 @@
 `include "defines.v"
 
 module imem(
-input  [`ADDR_SIZE-1:0]   a,
-output [`INSTR_SIZE-1:0]  rd
+    input  [`ADDR_SIZE-1:0]   addr,
+    output [`INSTR_SIZE-1:0]  data
 );
 
     reg [`INSTR_SIZE-1:0] RAM[`IMEM_SIZE-1:0];
@@ -14,28 +14,28 @@ output [`INSTR_SIZE-1:0]  rd
         $readmemh("riscv32_sim1.dat", RAM);
     end
 
-    assign rd = RAM[a[11:2]]; // instruction size aligned
+    assign data = RAM[addr[11:2]]; // instruction size aligned
 endmodule
 
 
 module dmem(
-input           	clk, we,
-input  [`XLEN-1:0]  a, wd,
-output [`XLEN-1:0]  rd
+    input                   clk, writeEnable,
+    input  [`WORD_LEN-1:0]  addr, writeData,
+    output [`WORD_LEN-1:0]  readData
 );
 
     reg  [31:0] RAM[1023:0];
 
-    assign rd = RAM[a[11:2]]; // word aligned
+    assign readData = RAM[addr[11:2]]; // word aligned
 
     always @(posedge clk)
-    if (we)
+    if (writeEnable)
     begin
-        RAM[a[11:2]] <= wd;   // store word
+        RAM[addr[11:2]] <= writeData;   // store word
         // DO NOT CHANGE THIS display LINE!!!
         // 不要修改下面这行display语句！！！
         /**********************************************************************/
-        $display("dataaddr = %h, memdata = %h", {a[31:2],2'b00}, RAM[a[11:2]]);
+        $display("dataaddr = %h, memdata = %h", {addr[31:2],2'b00}, RAM[addr[11:2]]);
         /**********************************************************************/
   	end
 endmodule
