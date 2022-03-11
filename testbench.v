7a9cdc06499fc2acb944b02398df347dbc5633fe
LSX-s-Software <2682191498@qq.com>
2022-03-11 20:07:49 +0800

    test: better debug

--- testbench.v
+++ testbench.v
@@ -23,18 +23,33 @@ module testbench();
 
         if (clk == 1'b1)
         begin
-            `ifdef DEBUG
+`ifdef DEBUG
             counter = counter + 1;
             $display("clock: %d", counter);
-            $display("pc_IF:\t%h", cpu.pc_IF);
-            $display("instr_IF:\t%h", cpu.instr_IF);
-            `endif
+`ifdef PIPELINING
+            $display("stage: IF      ID      EX      MEM      WB");
+            $display("PC:\t %h %h %h %h %h", cpu.pc_IF, cpu.pc_ID, cpu.pc_EX, cpu.pc_MEM, cpu.pc_WB);
+            $display("instr: %h %h", cpu.instr_IF, cpu.instr_ID);
+`else
+            $display("pc:\t%h", cpu.pc);
+            $display("instr:\t%h", cpu.instr);
+`endif
+`endif
+`ifdef PIPELINING
             if (cpu.pc_WB == 32'h00000078) // set to the address of the last instruction
             begin
                 $display("pc_WB:\t%h", cpu.pc_WB);
                 //$finish;
                 $stop;
             end
+`else
+            if (cpu.pc == 32'h00000078) // set to the address of the last instruction
+            begin
+                $display("pc:\t%h", cpu.pc);
+                //$finish;
+                $stop;
+            end
+`endif
         end
     end //end always
 endmodule
